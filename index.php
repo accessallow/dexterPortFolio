f628859e8070bc5904b556db778eac08a9a5691b
Pankaj Tiwari <pankajtiwarisagar@gmail.com>
2015-02-04 20:30:08 +0530

    first commit:panal1 is ready

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Portfolio</title>
+        <link rel="stylesheet" href='style.css'/>
+    </head>
+    <body>
+        <div id="first_section">
+            <img src="images/gola.png" id="kalagola"/>
+            
+            <div id="punchline">
+                <p>I am creating a beautiful Internet...</p>
+            </div>
+            
+            <div id="titlesection">
+                <h1>Pankaj Tiwari</h1>
+                <p>Web developer who rocked the world!!!</p>
+            </div>
+            
+            <div id="navigation">
+                <ul>
+                    <li><a href="#" class="active" style="opacity: 1;">Home</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Academics</a></li>
+                    <li><a href="#">Resume</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </div>
+            
+            <img src="images/dexter.png" id="chhotaDexter"/>
+            
+        </div>
+    </body>
+</html>

558d36808c95684cc46e0acb179912bb942c66f3
Pankaj Tiwari <pankajtiwarisagar@gmail.com>
2015-02-06 19:46:24 +0530

    Site font end is complete according to our design. Now thinking about its backend. Happy coding!!! Cheers :)

--- index.php
+++ index.php
@@ -32,5 +32,68 @@
             <img src="images/dexter.png" id="chhotaDexter"/>
             
         </div>
+        
+        <div id="secondsection">
+            <div class="projectpanel" id="panel1">
+                <img src="images/project_file.png"/>
+                <p>
+                    Hotel Project
+                </p>
+            </div>
+            <div class="projectpanel" id="panel2">
+                <img src="images/project_file.png"/>
+                <p>
+                    Hotel Project
+                </p>
+            </div>
+            <div class="projectpanel" id="panel3">
+                <img src="images/project_file.png"/>
+                <p>
+                    Hotel Project
+                </p>
+            </div>
+            <div class="projectpanel" id="panel4">
+                <img src="images/project_file.png"/>
+                <p>
+                    Hotel Project
+                </p>
+            </div>
+            <div class="projectpanel" id="panel5">
+                <img src="images/project_file.png"/>
+                <p>
+                    Hotel Project
+                </p>
+            </div>
+            
+            <div id="messageSection">
+                <h1>Perfection</h1>
+                <p>is my way of doing things...</p>
+                <a href="#">View My Projects</a>
+            </div>
+        </div>
+        
+        <div id="contactSection">
+            <img src="images/dexter_in_white_ellipse.png" id="happyDexter"/>
+            <form action="#" id="contactForm" method="POST">
+               <div class="control">
+                <label>Name</label>
+                <br/>
+                <input type="text"/>
+               </div>
+                <br/>
+                <div class="control">
+                 <label>Email</label>
+                 <br/>
+                <input type="text"/>
+                </div>
+                <br/>
+                <div class="control">
+                 <label>Message</label>
+                 <br/>
+                 <textarea></textarea>
+                </div>
+                 <input type="submit" value="Send"/>
+            </form>
+        </div>
     </body>
 </html>
